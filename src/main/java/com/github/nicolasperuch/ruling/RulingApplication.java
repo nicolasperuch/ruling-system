5cd40a6bf0918cfa93359aeef8e2e14200cc1da3
nicolasperuch <nicolas.peruch@southsystem.com.br>
2019-06-12 11:20:49 -0300

    Starting project

---
+++ src/main/java/com/github/nicolasperuch/ruling/RulingApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasperuch.ruling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulingApplication.class, args);
+	}
+
+}
